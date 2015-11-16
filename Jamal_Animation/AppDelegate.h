3f13e3cafc6898c5027131ce71985817735f8385
jamalping <lixiaoping@imac.local>
2015-11-16 10:16:58 +0800

    Initial Commit

---
+++ Jamal_Animation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Jamal_Animation
+//
+//  Created by jamalping on 15/11/16.
+//  Copyright © 2015年 cisc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Jamal_Animation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Jamal_Animation
+//
+//  Created by jamalping on 15/11/16.
+//  Copyright © 2015年 cisc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
